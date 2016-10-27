5b4c36168a3bf93bbac64b666270f704070468c3
Gasik Sambada <gasik@bigdutyfree.com>
2016-10-27 23:14:03 +0700

    finish

--- app/src/main/java/gan/obito/carta/carta/Input.java
+++ app/src/main/java/gan/obito/carta/carta/Input.java
@@ -2,9 +2,11 @@ package gan.obito.carta.carta;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.util.Log;
 import android.view.View;
 import android.view.Window;
@@ -16,6 +18,7 @@ import android.widget.RadioGroup;
 import com.squareup.okhttp.MediaType;
 import com.squareup.okhttp.RequestBody;
 
+import gan.obito.carta.carta.model.Hasil;
 import gan.obito.carta.carta.model.Pemeriksaan;
 import gan.obito.carta.carta.service.APIService;
 import retrofit.Call;
@@ -31,10 +34,10 @@ public class Input extends Activity implements View.OnClickListener {
     private Button bKirim;
     private ProgressDialog pDialog;
     private EditText EditNama,EditUsia,EditKolestrol,EditTekananDarah;
-    private Integer SelectedKelamin,SelectedMerokok,SelectedDiabetes;
+    private Integer SelectedKelamin,SelectedMerokok,SelectedDiabetes,ResultResikoID;
     private RadioGroup GroupKelamin,GroupMerokok,GroupDiabetes;
     private RadioButton EditKelamin,EditMerokok,EditDiabetes;
-    private String ValueKelamin,ValueMerokok,ValueDiabetes;
+    private String ValueKelamin,ValueMerokok,ValueDiabetes,ResultNama,ResultKodeWarna,ResultTingkatResiko,ResultDeskripsi;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -42,6 +45,8 @@ public class Input extends Activity implements View.OnClickListener {
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 
         setContentView(R.layout.input);
+
+
 
         EditNama = (EditText) findViewById(R.id.nama);
         EditUsia = (EditText) findViewById(R.id.usia);
@@ -73,15 +78,13 @@ public class Input extends Activity implements View.OnClickListener {
 
             case R.id.kirim:
                 calculateCarta();
-                //Intent intent_result = new Intent(this, Hasil.class);
-                //startActivity(intent_result);
                 break;
         }
     }
 
-    private void calculateCarta() {
+    private boolean calculateCarta() {
         showpDialog();
-
+        ResultNama = "";
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("http://stage.edutech.co.id/ptm-api/v1/")
                 .addConverterFactory(GsonConverterFactory.create())
@@ -130,6 +133,26 @@ public class Input extends Activity implements View.OnClickListener {
 
         }
 
+        if(EditNama.getText().toString().equals("")){
+            alertBox("Tolong isi nama anda");
+            hidepDialog();
+            return false;
+        }
+        if(EditUsia.getText().toString().equals("")){
+            alertBox("Tolong isi usia anda");
+            hidepDialog();
+            return false;
+        }
+        if(EditKolestrol.getText().toString().equals("")){
+            alertBox("Tolong isi tingkat kolestrol anda");
+            hidepDialog();
+            return false;
+        }
+        if(EditTekananDarah.getText().toString().equals("")){
+            alertBox("Tolong isi tingkat tekanan darah anda");
+            hidepDialog();
+            return false;
+        }
 
         Pemeriksaan Carta = new Pemeriksaan();
         Carta.setNama(EditNama.getText().toString());
@@ -157,15 +180,53 @@ public class Input extends Activity implements View.OnClickListener {
             @Override
             public void onResponse(Response<Pemeriksaan> response, Retrofit retrofit) {
                 Pemeriksaan pemeriksaan = response.body();
-                Log.d("Testing Raw Data","Success, Hasil : "+pemeriksaan.getResult().getTingkat_risiko());
+//                Log.d("Testing Raw Data","Success, Hasil : "+pemeriksaan.getStatus());
+                if(pemeriksaan.getStatus().equals("Error")){
+                    alertBox("Gagal : "+pemeriksaan.getError_description());
+                }else {
+                    ResultNama = pemeriksaan.getResult().getNama();
+                    ResultTingkatResiko = pemeriksaan.getResult().getTingkat_risiko();
+                    ResultKodeWarna = pemeriksaan.getResult().getKode_warna();
+                    ResultResikoID = pemeriksaan.getResult().getRisiko_id();
+                    ResultDeskripsi = pemeriksaan.getResult().getDeskripsi();
+
+                    Intent intent_result = new Intent(Input.this, Result.class);
+                    Bundle bQuestion = new Bundle();
+                    bQuestion.putString("nama", ResultNama);
+                    bQuestion.putInt("risiko_id", ResultResikoID);
+                    bQuestion.putString("kode_warna", ResultKodeWarna);
+                    bQuestion.putString("tingkat_risiko", ResultTingkatResiko);
+                    bQuestion.putString("deskripsi", ResultDeskripsi);
+                    intent_result.putExtras(bQuestion);
+                    startActivity(intent_result);
+                }
                 hidepDialog();
             }
 
             @Override
             public void onFailure(Throwable t) {
-                Log.d("Testing Raw Data","Error get Hasil : "+t.toString());
                 hidepDialog();
+                alertBox("Error get Hasil : "+t.toString());
             }
         });
+
+        return true;
+    }
+
+    public void alertBox(String message) {
+        AlertDialog.Builder alertBox = new AlertDialog.Builder(this);
+        alertBox.setMessage(message);
+        alertBox.setCancelable(true);
+
+        alertBox.setNegativeButton(
+                "OK",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+
+        AlertDialog alert = alertBox.create();
+        alert.show();
     }
 }
